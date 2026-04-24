d6d8a469fcefca2e1a2dc043ba2d308704cbef17
leonardecavele <leonard.decavele@gmail.com>
2026-04-24 14:46:42 +0200

    edit

--- src/defines.py
+++ src/defines.py
@@ -7,7 +7,7 @@ CHROMA_DIRECTORY = OUTPUT_DIRECTORY / "chroma"
 CHUNKS_METADATA_PATH = OUTPUT_DIRECTORY / "chunks_metadata.json"
 MANIFEST_PATH = OUTPUT_DIRECTORY / "manifest.json"
 
-MAX_BATCH_SIZE: int = 1024  # 1024
+MAX_BATCH_SIZE: int = 1024
 LLM_MODEL: str = (
     "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
     # "intfloat/multilingual-e5-small"
